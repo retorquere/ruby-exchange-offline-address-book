0a79841b97feea3ab7d38bd318e5cc480ccf60b8
retorquere <Emiliano.Heyns@iris-advies.com>
2017-06-17 09:43:48 +0200

    USERNAME env var is stricky

--- test/test_exchange-offline-address-book.rb
+++ test/test_exchange-offline-address-book.rb
@@ -3,19 +3,17 @@ require 'dotenv/load'
 require 'minitest/autorun'
 require 'exchange-offline-address-book'
 
-ENV['DEBUG'] = 'true'
-
 class EOABTest < Minitest::Test
   def test_downlod
-    oab = OfflineAddressBook.new(username: ENV['USERNAME'], password: ENV['PASSWORD'], email: ENV['EMAIL'])
+    oab = OfflineAddressBook.new(username: ENV['EWS_USERNAME'], password: ENV['EWS_PASSWORD'], email: ENV['EWS_EMAIL'])
     puts oab.addressbook
   end
   def test_baseurl
-    oab = OfflineAddressBook.new(username: ENV['USERNAME'], password: ENV['PASSWORD'], email: ENV['EMAIL'])
+    oab = OfflineAddressBook.new(username: ENV['EWS_USERNAME'], password: ENV['EWS_PASSWORD'], email: ENV['EWS_EMAIL'])
     puts oab.baseurl
   end
   def test_given_baseurl
-    oab = OfflineAddressBook.new(username: ENV['USERNAME'], password: ENV['PASSWORD'], email: ENV['EMAIL'], baseurl: ENV['BASEURL'])
+    oab = OfflineAddressBook.new(username: ENV['EWS_USERNAME'], password: ENV['EWS_PASSWORD'], email: ENV['EWS_EMAIL'], baseurl: ENV['EWS_BASEURL'])
     puts oab.addressbook
   end
 end
